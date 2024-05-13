b4f5c2c9c337238d637b9f79762074441209a8a3
charan2000 <charanbhavisetti2000@gmail.com>
2024-05-13 18:38:07 +0000

    NavBar finished { need to add own pics and titles }

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,165 +1,72 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import logo from "../../assets/logo.png";
+import { navItems } from "../../constants";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
-const navigation = [
-  { name: "Home", href: "current", current: true },
-  { name: "Services", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Clients", href: "#", current: false },
-];
+const Navbar = () => {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-export default function Example() {
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
+
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container px-4 mx-auto relative text-sm">
+        <div className="flex justify-between item-center">
+          <div className="flex items-center flex-shrink-0">
+            <img className="h-10 w-10 mr-2" src={logo}></img>
+            <span className="text-xl tracking-tight">Keerthi Realtors</span>
+          </div>
+          <ul className="hidden lg:flex ml-14 space-x-12 items-center">
+            {navItems.map((item, index) => (
+              <li key={item} href={item.href}>
+                {item.label}
+              </li>
+            ))}
+          </ul>
+          <div className="hidden lg:flex justify-center space-x-12 items-center">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              Sign In
+            </a>
+            <a
+              href="#"
+              className="bg-gradiant-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+            >
+              Create an Account
+            </a>
+          </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 flex flex-col w-full p-12 bg-neutral-900 justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="#" className="py-2 px-3 border rounded-md">
+                Sign In
+              </a>
+              <a
+                href="#"
+                className="py-2 px-3 bg-gradiant--to-r from-orange-300 to-orange-600 rounded-md"
+              >
+                Create an Account
+              </a>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+        )}
+      </div>
+    </nav>
   );
-}
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 const Home = () => {
-  return (<div>
-    <h1>Welcome to Home page ! </h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, repellat quisquam accusamus deleniti culpa facilis. Deleniti, rem! Facilis iusto adipisci doloribus obcaecati blanditiis minus quas est, illum ea quisquam id! Eveniet placeat ad quos tempora sunt rem at voluptates, pariatur illum dolore, ab aliquid similique. Dolorem fugiat omnis, officia at accusantium minima cumque numquam iure dolor magni voluptates! Asperiores quis saepe, ullam sint quam rem nobis repellendus pariatur? Incidunt id, non eius fugit expedita cumque molestiae aliquid tempore quod facere atque, eos saepe sequi perspiciatis quaerat voluptates voluptas officia harum quidem modi rerum? Voluptate placeat suscipit, facilis repudiandae voluptatibus pariatur!</p>
-  </div>;)
+  return (
+    <div>
+      <h1>Welcome to Home page ! </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, repellat
+        quisquam accusamus deleniti culpa facilis. Deleniti, rem! Facilis iusto
+        adipisci doloribus obcaecati blanditiis minus quas est, illum ea
+        quisquam id! Eveniet placeat ad quos tempora sunt rem at voluptates,
+        pariatur illum dolore, ab aliquid similique. Dolorem fugiat omnis,
+        officia at accusantium minima cumque numquam iure dolor magni
+        voluptates! Asperiores quis saepe, ullam sint quam rem nobis repellendus
+        pariatur? Incidunt id, non eius fugit expedita cumque molestiae aliquid
+        tempore quod facere atque, eos saepe sequi perspiciatis quaerat
+        voluptates voluptas officia harum quidem modi rerum? Voluptate placeat
+        suscipit, facilis repudiandae voluptatibus pariatur!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
